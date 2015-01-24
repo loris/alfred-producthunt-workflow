1e81a8b73aac50bebef7a57f559fcbf013acd5d7
Loris Guignard <loris.guignard@gmail.com>
2015-01-24 23:15:02 +0100

    updated workflow to use proxy server
    imp

--- source/server/index.js
+++ source/server/index.js
@@ -1,25 +1,42 @@
+/* global process */
 var http = require('http'),
   request = require('request');
 
-var opts = {
-  url: 'https://api.producthunt.com/v1/posts',
-  auth: {
-    bearer: process.env.PH_API_ACCESS_TOKEN
-  },
-  headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json',
-    'Accept-Encoding': ''
-  }
+var cachedPosts;
+
+var fetchPosts = function (callback) {
+  var opts = {
+    url: 'https://api.producthunt.com/v1/posts',
+    auth: {
+      bearer: process.env.PH_API_ACCESS_TOKEN
+    },
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  };
+
+  request.get(opts, function (err, resp, body) {
+    callback(err, body);
+  });
 };
 
-http.createServer(function (req, res) {
+(function loop() {
+  fetchPosts(function (err, posts) {
+    cachedPosts = posts;
+    setTimeout(function () {
+      loop();
+    }, 60*1000);
+  });
+})();
 
+http.createServer(function (req, res) {
   if (req.url === '/v1/posts') {
-    request.get(opts)
-      .pipe(res);
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(cachedPosts);
   } else {
-    res.writeHead(501);
-    res.end('Not Implemented');
+    res.writeHead(404);
+    res.end('Not Found');
   }
 }).listen(process.env.PORT || 3000);
+
